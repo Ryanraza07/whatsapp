a2f589d7a8134fc2c0262112d4ef5ae01dbff54b
Ryanraza07 <91653909+Ryanraza07@users.noreply.github.com>
2023-09-21 20:21:07 +0530

    Add files via upload

---
+++ whatsappbaba.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit
+
+
+# In[2]:
+
+
+pywhatkit.sendwhatmsg_to_group_instantly("Igm4MjDccoLAxrHxzjkm1P","hello bhai log")
+
+
+# In[ ]:
+
+
+
+

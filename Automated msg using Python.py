51ab26ef08042297e47af73f0db1487c7b87d44e
Ryanraza07 <91653909+Ryanraza07@users.noreply.github.com>
2023-12-31 20:45:29 +0530

    Add files via upload

---
+++ Automated msg using Python.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install pywhatkit --upgrade
+
+
+# In[2]:
+
+
+import pywhatkit
+
+
+# In[3]:
+
+
+#pywhatkit.sendwhatmsg_instantly("+917905131362","hello")
+
+
+# In[5]:
+
+
+pywhatkit.sendwhatmsg("+917905131362","hello",20,44)
+
+
+# In[ ]:
+
+
+
+
